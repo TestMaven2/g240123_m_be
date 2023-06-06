1923d72f95e0811402827e4c6d5046751f53d233
Andrey <k-udm@ya.ru>
2023-06-06 12:49:21 +0200

    Lesson 14

---
+++ src/main/java/lesson14/task1/Utils.java
@@ -0,0 +1,19 @@
+package lesson14.task1;
+
+public class Utils {
+
+    public static void printGraph(Graph graph) {
+        int source = 0;
+        int size = graph.getList().size();
+
+        if (source < size) {
+            do {
+                for (int dest : graph.getList().get(source)) {
+                    System.out.print("[" + source + " —> " + dest + "]");
+                }
+                System.out.println();
+                source++;
+            } while (source < size);
+        }
+    }
+}
